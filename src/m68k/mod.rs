79de036c45b8cdbf0d48ee8b665f7e3a12d03124
Michael Westbom <mike@totallymike.org>
2016-09-29 11:28:31 -0400

    More opcodes, slightly better debugging

--- src/m68k/mod.rs
+++ src/m68k/mod.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 bitflags! {
   pub flags SizeField: u16 {
     const BYTE = 0b00 << 6,
@@ -8,6 +10,56 @@ bitflags! {
 bitflags! {
   pub flags OpCodes: u16 {
     const TST = 0b01001010 << 8,
+    const TST_W = TST.bits | WORD.bits,
     const TST_L = TST.bits | LONG.bits,
+    const BNE = 0b01100110 << 8,
+  }
+}
+
+impl fmt::Display for OpCodes {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match *self {
+      TST_L => write!(f, "tst.l"),
+      TST_W => write!(f, "tst.w"),
+      BNE => write!(f, "bne"),
+      _ => write!(f, "instruction not found"),
+    }
+  }
+}
+
+bitflags! {
+  flags ConditionCodes: u16 {
+    const CARRY = 0b01,
+    const OVERFLOW = 0b10,
+    const ZERO = 0b100,
+    const EXTEND = 0b1000,
+  }
+}
+
+pub struct M68k {
+  cc_register: u16,
+  pub pc_register: u16,
+  data_registers: [u16; 8],
+  address_registers: [u16; 8],
+}
+
+impl M68k {
+  pub fn new() -> M68k {
+    M68k {
+      cc_register: 0,
+      pc_register: 0,
+      data_registers: [0; 8],
+      address_registers: [0; 8],
+    }
+  }
+
+  pub fn set_zero_flag(&mut self) {
+    self.cc_register = ZERO.bits;
+  }
+
+  pub fn zero(&self) -> bool {
+    ConditionCodes::from_bits(self.cc_register)
+      .unwrap()
+      .contains(ZERO)
   }
 }
--- src/system/mod.rs
+++ src/system/mod.rs
@@ -1,18 +1,72 @@
 use std::io::prelude::*;
 use std::io::Cursor;
 use std::io::SeekFrom;
+use std::result;
 use byteorder::{BigEndian,ReadBytesExt,WriteBytesExt};
 
 use rom;
+use m68k;
+use m68k::OpCodes;
+use m68k::M68k;
 
 pub struct System {
+  pub cpu: M68k,
   pub memory: Box<[u8]>,
 }
 
 impl System {
   pub fn new() -> System {
     System {
+      cpu: M68k::new(),
       memory: vec![0; 0x1FFFFFE].into_boxed_slice(),
+    }
+  }
+
+  pub fn process_next(system: System) -> Result<System, &'static str> {
+    let mut system = system;
+    match system.process_next_instruction() {
+      Ok(true) => Ok(system),
+      Err(err) => Err(err),
+      _ => Err("something happened")
+    }
+  }
+
+  pub fn process_next_instruction(&mut self) -> result::Result<bool, &'static str> {
+    let instruction = self.read_next_word();
+    let opcode = OpCodes::from_bits_truncate(instruction);
+
+    println!("instruction: {}", opcode);
+    println!("opcode {:#018b}", instruction);
+    match opcode {
+      m68k::TST_L => {
+        let address_high: u32 = self.read_next_word() as u32;
+        let address_low = self.read_next_word();
+        let address_high = address_high << 16;
+        let address: usize = address_high as usize + address_low as usize;
+        let operand: u16 = self.read_memory_address(address);
+        self.set_cpu_flags(operand);
+        println!("val in operand {:010x}", operand);
+        Ok(true)
+      }
+      m68k::TST_W => {
+        let address = self.read_next_word() as usize;
+        let operand: u16 = self.read_memory_address(address);
+        println!("val in operand {:010x}", operand);
+        self.set_cpu_flags(operand);
+        Ok(true)
+      }
+      m68k::BNE => {
+        if !self.cpu.zero() {
+          let address = instruction as u8;
+          if address != 0xFF && address != 0x0 {
+            let new_address = self.cpu.pc_register + address as u16;
+            println!("{:#x}", new_address);
+            self.cpu.pc_register += address as u16;
+          }
+        }
+        Ok(true)
+      }
+      _ => { Err("instruction not implemented") }
     }
   }
 
@@ -25,6 +79,27 @@ impl System {
       cursor.write_u16::<BigEndian>(n).expect("Write failed!");
     }
 
-    System { memory: cursor.into_inner() }
+    System { memory: cursor.into_inner(), ..system }
+  }
+
+  fn read_next_word(&mut self) -> u16 {
+    let address = self.cpu.pc_register as usize;
+    self.cpu.pc_register += 2;
+    self.read_memory_address(address)
+  }
+
+  fn read_memory_address(&self, address: usize) -> u16 {
+    println!("READ FROM ${:06x}", address);
+    use std::mem;
+    let memory = &self.memory;
+    let val: u16 = unsafe { mem::transmute([memory[address], memory[address + 1]]) };
+    u16::from_be(val)
+  }
+
+  fn set_cpu_flags(&mut self, operand: u16) {
+    match operand {
+      0u16 => { self.cpu.set_zero_flag(); },
+      _ => { }
+    }
   }
 }
