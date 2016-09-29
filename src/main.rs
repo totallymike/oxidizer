d74ca6a2d99ff42df8dfe3717fdb9110d44b2c45
Michael Westbom <mike@totallymike.org>
2016-09-29 11:28:49 -0400

    Cleanup, use loops, some other stuff

--- src/main.rs
+++ src/main.rs
@@ -13,38 +13,32 @@ mod m68k;
 
 mod system;
 
-use byteorder::{BigEndian,ReadBytesExt};
-use std::io::Cursor;
-use std::io::prelude::*;
-use std::io::SeekFrom;
 use system::System;
 
 fn main() {
   // gui::run();
-  let system = {
+  let mut system = {
     let system = system::System::new();
     let rom = rom::Rom::new("sonic.bin").unwrap();
 
     System::load_rom(system, rom)
   };
   
-  println!("|{:#010b}|", system.memory[0x206]);  
-  let mut my_cursor = Cursor::new(system.memory);
+  system.cpu.pc_register = 0x206;
+  let mut result = System::process_next(system);
+  while let Ok(system) = result {
+    result = System::process_next(system);
+  }
 
-  my_cursor.seek(SeekFrom::Start(0x206)).expect("Couldn't seek!");
-  println!("|{:#018b}|", my_cursor.read_u16::<BigEndian>().unwrap());
-  println!("|{:#010x}|", my_cursor.read_u16::<BigEndian>().unwrap());
-  println!("|{:#010x}|", my_cursor.read_u16::<BigEndian>().unwrap());
+  if let Err(err) = result {
+    println!("{}", err);
+  }
+
   // println!("|{}|", rom.system_name);
   // println!("|{}|", rom.domestic_name);
   // println!("{:#010x}", rom.rom_start);
   // println!("{:#010X}", rom.rom_end);
   // println!("|{}|", rom.control_data);
-  // let mut rom = rom;
-  // let inst = rom.first_instruction();
-  // let opcode = m68k::OpCodes::from_bits_truncate(inst);
-  // if opcode == m68k::TST_L {
-  //   println!("Yep");
-  // }
+
   // println!("|{:#018b}|", inst);
 }
